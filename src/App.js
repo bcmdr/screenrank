8c7841f86e7680ec8823b84e4c996f58359e5100
Brett Commandeur <bcmdr.co@gmail.com>
2022-12-06 22:00:56 -0700

    begin setting up tmdb connection

--- src/App.js
+++ src/App.js
@@ -1,86 +1,111 @@
 // import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from "react";
-import { EmailAuthProvider, onAuthStateChanged, sendSignInLinkToEmail, isSignInWithEmailLink, signInWithEmailLink  } from "firebase/auth";
-import { auth } from './firebaseConfig';
 
-function LoginControl() {
-
-  const actionCodeSettings = {
-    // URL you want to redirect back to. The domain (www.example.com) for this
-    // URL must be in the authorized domains list in the Firebase Console.
-    url: 'http://localhost:3000',
-    // This must be true.
-    handleCodeInApp: true,
-    iOS: {
-      bundleId: 'com.example.ios'
-    },
-    android: {
-      packageName: 'com.example.android',
-      installApp: true,
-      minimumVersion: '12'
-    },
-    dynamicLinkDomain: 'screenrank.page.link'
-  };
-
-  const handleSignIn = async () => {
-    try {
-      await sendSignInLinkToEmail(auth, email, actionCodeSettings);
-      window.localStorage.setItem('emailForSignIn', email);
-    } catch (error) {
-      alert(error);
-    }
-  }
-
-  return (
-    <>
-      <button onClick={handleSignIn}>Sign In</button>
-    </>
-  )
-}
-
-const email = "bcommandeur@gmail.com";
-
-
+let id = 1;
 
 function App() {
-  const [user, setUser] = useState({}); 
-
-  useEffect(() => {
-    console.log('Effect on Load');
-    if (isSignInWithEmailLink(auth, window.location.href)) {
-      // Get the email if available. This should be available if the user completes
-      // the flow on the same device where they started it.
-      let email = window.localStorage.getItem('emailForSignIn');
-      if (!email) {
-        // User opened the link on a different device. To prevent session fixation
-        // attacks, ask the user to provide the associated email again. For example:
-        email = window.prompt('Please provide your email for confirmation');
-      }
-      // The client SDK will parse the code from the link for you.
-      signInWithEmailLink(auth, email, window.location.href);
+  const [currentListId, setCurrentListId] = useState(() => {
+    let savedCurrentListId = localStorage.getItem('currentListId');
+    if (savedCurrentListId) {
+      return JSON.parse(savedCurrentListId);
+    } else {
+      return 1;
     }
-  }, []);
+  })
+
+  const [currentList, setCurrentList] = useState(() => {
+    let savedList = localStorage.getItem('list-' + currentListId);
+    if (savedList) {
+      return JSON.parse(savedList);
+    } else {
+      return {
+        id: 1,
+        name: 'New List', 
+        items: []
+      };
+    }
+  });
+
+  const [tmdbConfig, setTmdbConfig] = useState({});
+  const [localeOptions, setLocaleOptions] = useState([]);
+  const savedLocale = localStorage.getItem('locale');
+  const [locale, setLocale] = useState(
+    savedLocale ? savedLocale : 'CA'
+  );
 
   useEffect(() => {
-    onAuthStateChanged(auth, 
-      (user) => {
-        if (user) {
-          setUser(user);
-          console.log(user);
-        } else {
-          setUser({});
-        }
-      }
-    )
+    localStorage.setItem(`list-${currentList.id}`, JSON.stringify(currentList));
+  }, [currentList]);
+
+  useEffect(() => {
+    window.localStorage.setItem('locale', locale);
+  }, [locale]);
+
+  useEffect(() => {
+    const fetchConfigData = async () => {
+      const response = await fetch(
+        'https://api.themoviedb.org/3/configuration?api_key=251ba64a492fa521304db43e5fa3d2ad',
+      );
+      const data = await response.json();
+      setTmdbConfig(data);
+    };
+    const fetchProviderData = async () => {
+      const response = await fetch(
+        'https://api.themoviedb.org/3/watch/providers/regions?api_key=251ba64a492fa521304db43e5fa3d2ad&language=en-US',
+      );
+      const data = await response.json();
+      setLocaleOptions(data.results);
+    };
+ 
+    fetchConfigData();
+    fetchProviderData();
   }, []);
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    let title = event.target.title.value;
+    event.target.title.value = "";
+    searchMovies(title, currentList.items.length);
+  }
+
+  const searchMovies = (title, rank) => {
+    saveMovie({
+      id: id++,
+      title,
+      rank
+    })
+  }
+
+  const saveMovie = (movieData, rank) => {
+    let newList = {...currentList};
+    newList.items.push(movieData);
+    setCurrentList(newList);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <div>{user.name}</div>
-        <LoginControl></LoginControl>
+        <h1>ScreenRank</h1>
       </header>
+      <main>
+        <article>
+          <header>
+            <h2>{currentList.name}</h2>
+          </header>
+          <ol className="movie-list">
+            {currentList.items.map((item, index) => (
+              <li className="movie-list__item" key={index}>{item.title}</li>
+            ))}
+            <li className="movie-search">
+              <form id="movie-search__form" onSubmit={handleSearchSubmit}>
+                <input name="title"></input>
+                <button type="submit">Search</button>
+              </form>
+            </li>
+          </ol>
+        </article>
+      </main>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,18 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-onAuthStateChanged(auth, 
-  (user) => {
-    if (user) {
-      setUser(user);
-      console.log(user);
-    } else {
-      setUser({});
-    }
-  }
-)
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
